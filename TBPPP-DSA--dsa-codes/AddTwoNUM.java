3857a142b8622370bc80c4776dcbdd5001770738
Chetna1405 <122478476+Chetna1405@users.noreply.github.com>
2024-11-12 16:58:23 +0530

    commit

--- TBPPP-DSA--dsa-codes/AddTwoNUM.java
+++ TBPPP-DSA--dsa-codes/AddTwoNUM.java
@@ -1,34 +1,51 @@
+import org.w3c.dom.Node;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+
+    }
+
+    ListNode(int val) {
+        val = val;
+        next = null;
+    }
+
+}
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result=new ListNode();
-        ListNode current=result;
-        int carry=0;
-        while(l1!=null||l2!=null||carry!=0){
-            int sum=carry;
-            if(l1!=null){
-                sum+=l1.val;
-                l1=l1.next;
+        ListNode result = new ListNode();
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
             }
-            if(l2!=null){
-                sum+=l2.val;
-                l2=l2.next;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
-            carry=sum/10;
-            sum=sum%10;
-            current.next=new ListNode(sum);
-            current=current.next;
+            carry = sum / 10;
+            sum = sum % 10;
+            current.next = new ListNode(sum);
+            current = current.next;
         }
         return result.next;
-   
+
     }
 }
